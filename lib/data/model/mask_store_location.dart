55872bc81b097b6ac94e8b7dfb92f6cb883897c9
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-08-25 20:27:39 +0900

    마스크 상점 위치 model

---
+++ lib/data/model/mask_store_location.dart
@@ -0,0 +1,11 @@
+ class MaskStoreLocation {
+  // 위도
+  final double latitude;
+  // 경도
+  final double longitude;
+
+  const MaskStoreLocation({
+    required this.latitude,
+    required this.longitude,
+  });
+}
