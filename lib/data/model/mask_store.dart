f8334d3ec0fa02bf1f271277f29c3123f5dac63c
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-08-25 20:26:59 +0900

    마스크 상점 정보 모델클래스

---
+++ lib/data/model/mask_store.dart
@@ -0,0 +1,13 @@
+class MaskStore {
+  final String storeName;
+  final String address;
+  final String distance;
+  final String remainStatus;
+
+  const MaskStore({
+    required this.storeName,
+    required this.address,
+    required this.distance,
+    required this.remainStatus,
+  });
+}
