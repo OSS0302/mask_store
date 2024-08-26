46dd1449d04a8b9efe993fad60b747cf1aba1c4c
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-08-26 15:22:37 +0900

    마스크 상점 레포지 토리 변경

--- lib/data/repository/mask_store_location_repository.dart
+++ lib/data/repository/mask_store_location_repository.dart
@@ -1,5 +1,12 @@
-import 'package:mask_store/data/model/mask_store_location.dart';
+import 'package:mask_store/data/model/my_location.dart';
 
-abstract interface class MaskStoreLocationRepository {
-  Future<MaskStoreLocation> getMaskStoreLocation();
+abstract interface class MyLocationRepository {
+  Future<MyLocation> getMyLocation();
+
+  double distanceBetween(
+      double startLat,
+      double startLng,
+      double endLat,
+      double endLng,
+      );
 }
