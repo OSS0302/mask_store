8fcb5ce9806cfc9c5241bb7b87c24775f904d0ba
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-08-26 15:23:25 +0900

    maskstore viewModel 추가

---
+++ lib/ui/mask_store_view_model.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:mask_store/data/model/mask_store.dart';
+import 'package:mask_store/data/repository/mask_store_location_repository.dart';
+import 'package:mask_store/data/repository/mask_store_repository.dart';
+
+class MaskStoreViewModel extends ChangeNotifier {
+  final MaskStoreRepository _maskStoreRepository;
+  final MyLocationRepository _myLocationRepository;
+
+  MaskStoreViewModel({
+    required MaskStoreRepository maskStoreRepository,
+    required MyLocationRepository myLocationRepository,
+  })  : _maskStoreRepository = maskStoreRepository,
+        _myLocationRepository = myLocationRepository;
+
+  // 변수 상태 관리
+  List<MaskStore> _stores = [];
+
+  List<MaskStore> get stores => List.unmodifiable(_stores);
+
+  bool _isLoading = false;
+
+  bool get isLoading => _isLoading;
+
+  Future<void> fetchStores() async {
+    _isLoading = true;
+    notifyListeners();
+
+    final stores = await _maskStoreRepository.getStoreInfo();
+    final myLocation = await _myLocationRepository.getMyLocation();
+
+    for (var store in stores) {
+      store.distance = _myLocationRepository.distanceBetween(
+        store.latitude,
+        store.longitude,
+        myLocation.latitude,
+        myLocation.longitude,
+      );
+    }
+
+    // 정렬
+    stores.sort((store, my)=> store.distance.compareTo(my.distance));
+    _stores = stores;
+
+    _isLoading = false;
+    notifyListeners();
+  }
+}
