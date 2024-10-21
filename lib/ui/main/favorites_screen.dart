f56890cad720a0721b01ff475d85447d6b99fe02
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-10-21 20:16:51 +0900

    즐겨찾기 검색 기능 추가

--- lib/ui/main/favorites_screen.dart
+++ lib/ui/main/favorites_screen.dart
@@ -4,17 +4,24 @@ import 'package:provider/provider.dart';
 import '../../data/model/mask_store.dart';
 import '../component/store_item.dart';
 
-class FavoritesScreen extends StatelessWidget {
+class FavoritesScreen extends StatefulWidget {
   const FavoritesScreen({Key? key}) : super(key: key);
+
+  @override
+  _FavoritesScreenState createState() => _FavoritesScreenState();
+}
+
+class _FavoritesScreenState extends State<FavoritesScreen> {
+  final TextEditingController _searchController = TextEditingController();
+  String _searchQuery = '';
 
   @override
   Widget build(BuildContext context) {
     final maskStoreViewModel = context.watch<MaskStoreViewModel>();
-    final favoriteStores = maskStoreViewModel.state.stores
-        .where((store) => store.isFavorite)
-        .toList();
 
-    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    final favoriteStores = maskStoreViewModel.state.stores
+        .where((store) => store.isFavorite && store.storeName.contains(_searchQuery))
+        .toList();
 
     return Scaffold(
       appBar: AppBar(
@@ -26,45 +33,69 @@ class FavoritesScreen extends StatelessWidget {
           ),
         ),
         centerTitle: true,
-        backgroundColor: isDarkMode ? Colors.black : Colors.teal.shade300,
+        backgroundColor: Colors.teal.shade300,
         elevation: 0,
       ),
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: isDarkMode
-                ? [Colors.black, Colors.grey.shade900]
-                : [Colors.teal.shade100, Colors.teal.shade50],
+            colors: [Colors.teal.shade100, Colors.teal.shade50],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
           ),
         ),
-        child: favoriteStores.isEmpty
-            ? _buildEmptyFavorites(isDarkMode)
-            : Padding(
-          padding: const EdgeInsets.symmetric(
-              horizontal: 16.0, vertical: 10.0),
-          child: ListView.builder(
-            physics: const BouncingScrollPhysics(), // 스크롤 탄성 효과
-            itemCount: favoriteStores.length,
-            itemBuilder: (context, index) {
-              final store = favoriteStores[index];
-              return _buildStoreCard(store, context, isDarkMode);
-            },
-          ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _searchController,
+                onChanged: (value) {
+                  setState(() {
+                    _searchQuery = value; // 검색어 업데이트
+                  });
+                },
+                decoration: InputDecoration(
+                  hintText: '약국 이름 검색',
+                  prefixIcon: const Icon(Icons.search, color: Colors.teal),
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: favoriteStores.isEmpty
+                  ? _buildEmptyFavorites()
+                  : Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+                child: ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: favoriteStores.length,
+                  itemBuilder: (context, index) {
+                    final store = favoriteStores[index];
+                    return _buildStoreCard(store, context);
+                  },
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 
-  Widget _buildEmptyFavorites(bool isDarkMode) {
+  Widget _buildEmptyFavorites() {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Icon(
             Icons.favorite_border,
-            color: isDarkMode ? Colors.grey.shade600 : Colors.grey.shade400,
+            color: Colors.grey.shade400,
             size: 100,
           ),
           const SizedBox(height: 16),
@@ -72,7 +103,7 @@ class FavoritesScreen extends StatelessWidget {
             '즐겨찾기 한 약국이 없습니다.',
             style: TextStyle(
               fontSize: 20,
-              color: isDarkMode ? Colors.grey.shade500 : Colors.grey.shade600,
+              color: Colors.grey.shade600,
               fontWeight: FontWeight.w600,
             ),
           ),
@@ -81,13 +112,12 @@ class FavoritesScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildStoreCard(MaskStore store, BuildContext context, bool isDarkMode) {
+  Widget _buildStoreCard(MaskStore store, BuildContext context) {
     return GestureDetector(
       onTap: () {
         // 약국 상세 페이지로 이동하거나 다른 동작 추가 가능
       },
       child: Card(
-        color: isDarkMode ? Colors.grey.shade800 : Colors.white,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
@@ -119,27 +149,19 @@ class FavoritesScreen extends StatelessWidget {
                 children: [
                   Text(
                     store.storeName,
-                    style: TextStyle(
+                    style: const TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
-                      color: isDarkMode ? Colors.white : Colors.black,
                     ),
                   ),
                   const SizedBox(height: 8),
                   Row(
                     children: [
-                      Icon(Icons.location_on,
-                          color: isDarkMode
-                              ? Colors.teal.shade200
-                              : Colors.teal.shade300),
+                      Icon(Icons.location_on, color: Colors.teal.shade300),
                       const SizedBox(width: 4),
                       Text(
                         '${store.distance.toStringAsFixed(2)} km',
-                        style: TextStyle(
-                          color: isDarkMode
-                              ? Colors.grey.shade400
-                              : Colors.grey.shade600,
-                        ),
+                        style: TextStyle(color: Colors.grey.shade600),
                       ),
                     ],
                   ),
@@ -153,14 +175,10 @@ class FavoritesScreen extends StatelessWidget {
                           store.isFavorite
                               ? Icons.favorite
                               : Icons.favorite_border,
-                          color: store.isFavorite
-                              ? Colors.red
-                              : (isDarkMode ? Colors.white : Colors.grey),
+                          color: store.isFavorite ? Colors.red : Colors.grey,
                         ),
                         onPressed: () {
-                          context
-                              .read<MaskStoreViewModel>()
-                              .toggleFavorite(store);
+                          context.read<MaskStoreViewModel>().toggleFavorite(store);
                         },
                       ),
                     ],
